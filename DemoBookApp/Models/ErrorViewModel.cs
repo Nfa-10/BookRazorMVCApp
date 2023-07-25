87f93b3c4b04d0725c747770281c5d55b6f668fe
Nafeesa Arif <nafeesa.arif@techno-soft.com>
2023-07-25 13:53:45 +0500

    Project Creation

---
+++ DemoBookApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoBookApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
