71ff5e87d031913b80e34d49941da10b4c26cfb6
nafeesa.arif <nafeesa.arif@TS-NAFEESA-PC>
2023-08-01 15:34:29 +0500

    Removed Comments

--- DemoBookApp/Data/BookDbContext.cs
+++ DemoBookApp/Data/BookDbContext.cs
@@ -71,58 +71,6 @@ namespace DemoBookApp.Data
             }
         }
 
-
-        /*public override int SaveChanges()
-        {
-            AddTimestamps();
-            return base.SaveChanges();
-        }
-
-    public virtual async Task<int> SaveChangesAsync()
-    {
-            AddTimestamps();
-            return await base.SaveChangesAsync();
-        }
-
-    private void AddTimestamps()
-    {
-        var entities = ChangeTracker.Entries()
-            .Where(x => x.Entity is Auditable && 
-            (x.State == EntityState.Added || x.State == EntityState.Modified));
-
-
-
-        foreach (var entity in entities)
-        {
-            if (entity.State == EntityState.Added)
-            {
-                ((Auditable)entity.Entity).DateCreated = DateTime.UtcNow;
-
-            }
-         ((Auditable)entity.Entity).DateModified = DateTime.UtcNow;
-
-        }
-    }*/
-        /*public override int SaveChanges()
-    {
-        var entries = ChangeTracker
-            .Entries()
-            .Where(e => e.Entity is Auditable && (
-                    e.State == EntityState.Added
-                    || e.State == EntityState.Modified));
-
-        foreach (var entityEntry in entries)
-        {
-            ((Auditable)entityEntry.Entity).DateUpdated = DateTime.Now;
-
-            if (entityEntry.State == EntityState.Added)
-            {
-                ((Auditable)entityEntry.Entity).DateCreated = DateTime.Now;
-            }
-        }
-
-        return base.SaveChanges();
-    }*/
         public DbSet<BookModel> Books { get; set; }
         public DbSet<AuthorModel> Author { get; set; } 
 
